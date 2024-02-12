17d63c86fbb13fced2c5a23e02f84d2de74e981a
joshuailevy <joshua.levy16@gmail.com>
2024-02-12 11:33:17 -0800

    ran demix step with updated barcodes

--- scripts/demix_parallel.sh
+++ scripts/demix_parallel.sh
@@ -9,12 +9,18 @@ my_func() {
     baseName=${baseName%.*}
     depthfile0="${depthfolder}${fn_out%.tsv}.depths"
     output0="${output}${fn_out%.*}.demix.tsv"
-    # echo $depthfile0
     echo $fn
     echo $depthfile0
     echo $output0
-    # freyja demix $fn $depthfile0 --output $output0 --eps 0.0000001
+    freyja demix $fn $depthfile0 --output $output0 --eps 0.0000001
+    # if [ -f "$output0" ]; then
+    #     echo "file already exists. skipping. "
+    # else
+    #     # echo "file doesn't exist. running. "
+    #     freyja demix $fn $depthfile0 --output $output0 --eps 0.0000001
+    # fi
+
 }
 
 export -f my_func
-parallel -j 4 my_func ::: ../variants/* ::: ../depths/ ::: ../outputs/
+parallel -j 24 my_func ::: ../variants/* ::: ../depths/ ::: ../outputs/
